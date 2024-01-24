d9309a03209a06b0c71b330e1fc25560a6e2e0d7
Omnonny <142103983+Omnonny@users.noreply.github.com>
2024-01-23 20:21:54 -0500

    Updated logic in Organizer.py

--- Organizer.py
+++ Organizer.py
@@ -20,6 +20,16 @@ def organize_files(source_dir, destination_dir, config):
     for filename in os.listdir(source_dir):
         source_path = os.path.join(source_dir, filename)
         
+        if file_extension in file_types:
+                type_folder = os.path.join(destination_dir, file_types[file_extension])
+            else:
+                type_folder = os.path.join(destination_dir, 'other')
+            if not os.path.exists(type_folder):
+                os.makedirs(type_folder)
+            destination_path = os.path.join(type_folder, filename)
+            shutil.move(source_path, destination_path)
+            print(f'Moved: {filename} to {type_folder}')
+        
 
 
 
